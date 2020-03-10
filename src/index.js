206a4d0ef6d1842bf0ceadc196d6319dada74c00
volha.mashnitskaya <volha.mashnitskaya@gmail.com>
2020-03-10 23:49:22 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    let openingBrackets = bracketsConfig.map(element => element[0]);
+    let closingBrackets = bracketsConfig.map(element => element[1]);
+
+    let stack = [];
+
+    for (let i = 0; i < str.length; i++) {
+
+        if (checkOpenBracket(str[i], openingBrackets) && checkOpenBracket(str[i], closingBrackets)) {
+            if (!stack.includes(str[i])) {
+                stack.push(str[i]);
+            } else if (stack.pop() != str[i]) {
+                return false;
+            }
+        }
+
+
+        if (checkOpenBracket(str[i], openingBrackets)) {
+            stack.push(str[i]);
+        }
+
+
+        if (checkOpenBracket(str[i], closingBrackets)) {
+            if (stack.length == 0 || stack.pop() != openingBrackets[closingBrackets.indexOf(str[i])]) return false;
+        }
+    }
+
+    return stack.length === 0;
+}
+
+
+function checkOpenBracket(str, openingArray) {
+    return (openingArray.includes(str)) ? true : false;
 }
